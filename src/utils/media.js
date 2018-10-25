ecd171e515886d64a2a40321e19fb3c7f0c5fd26
John Betancur <johnnyazee@gmail.com>
2018-10-24 20:27:43 -0400

    fix lint errors

--- src/utils/media.js
+++ src/utils/media.js
@@ -3,53 +3,53 @@ import { css } from 'styled-components';
 export const media = {
   flex: (...args) => css`
     ${css(...args)}
-    `,
+  `,
   xs: (...args) => css`
     @media (min-width: 0) and (max-width: 599px) {
-    ${css(...args)}
+      ${css(...args)}
     }
-    `,
+  `,
   sm: (...args) => css`
     @media (min-width: 600px) and (max-width: 959px) {
-    ${css(...args)}
+      ${css(...args)}
     }
-    `,
+  `,
   md: (...args) => css`
     @media (min-width: 960px) and (max-width: 1280px) {
-    ${css(...args)}
+      ${css(...args)}
     }
-    `,
+  `,
   lg: (...args) => css`
     @media only screen and (min-width: 1280px) {
-    ${css(...args)}
+      ${css(...args)}
     }
-    `
+  `
 };
 
 export const fluid = {
   xs: (...args) => css`
     ${css(...args)}
-    `,
+  `,
   greaterXS: (...args) => css`
     @media (min-width: 34em) {
-    ${css(...args)}
+      ${css(...args)}
     }
-    `,
+  `,
   greaterSM: (...args) => css`
     @media (min-width: 48em) {
-    ${css(...args)}
+      ${css(...args)}
     }
-    `,
+  `,
   greaterMD: (...args) => css`
     @media (min-width: 62em) {
-    ${css(...args)}
+      ${css(...args)}
     }
-    `,
+  `,
   greaterLG: (...args) => css`
     @media (min-width: 75em) {
-    ${css(...args)}
+      ${css(...args)}
     }
-    `
+  `
 };
 
 export const genWidth = (prop, divisions, gutter) => `calc(${(prop / divisions) * 100}% - ${gutter * 2}px)`;
